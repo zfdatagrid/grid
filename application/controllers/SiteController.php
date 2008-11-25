8b90adb02f8618294227f9bde12b2adb162a7276
Bento Vilas Boas <bento@licentia.pt>
2008-11-25 05:20:22 +0000

    Code optimization

--- application/controllers/SiteController.php
+++ application/controllers/SiteController.php
@@ -71,53 +71,45 @@ class SiteController extends Zend_Controller_Action
         switch ($export)
         {
             case 'odt' :
-                $grid = new Bvb_Grid_Deploy_Odt ( $db, 'Grid Example', 'media/temp', array ('download' ) );
-                #$grid->cache = array('use'=>0,'instance'=>Zend_Registry::get('cache'),'tag'=>'grid');
+                $grid = "Bvb_Grid_Deploy_Odt";
                 break;
             case 'ods' :
-                $grid = new Bvb_Grid_Deploy_Ods ( $db, 'Grid Example', 'media/temp', array ('download' ) );
-                #$grid->cache = array('use'=>0,'instance'=>Zend_Registry::get('cache'),'tag'=>'grid');
+                $grid = "Bvb_Grid_Deploy_Ods";
                 break;
             case 'xml' :
-                $grid = new Bvb_Grid_Deploy_Xml ( $db, 'Grid Example', 'media/temp', array ('download' ) );
-                #$grid->cache = array('use'=>0,'instance'=>Zend_Registry::get('cache'),'tag'=>'grid');
+                $grid = "Bvb_Grid_Deploy_Xml";
                 break;
             case 'csv' :
-                $grid = new Bvb_Grid_Deploy_Csv ( $db, 'Grid Example', 'media/temp', array ('download' ) );
-                #$grid->cache = array('use'=>0,'instance'=>Zend_Registry::get('cache'),'tag'=>'grid');
+                $grid = "Bvb_Grid_Deploy_Csv";
                 break;
             case 'excel' :
-                $grid = new Bvb_Grid_Deploy_Excel ( $db, 'Grid Example', 'media/temp', array ('download' ) );
-                #$grid->cache = array('use'=>0,'instance'=>Zend_Registry::get('cache'),'tag'=>'grid');
+                $grid = "Bvb_Grid_Deploy_Excel";
                 break;
             case 'word' :
-                $grid = new Bvb_Grid_Deploy_Word ( $db, 'Grid Example', 'media/temp', array ('download' ) );
-                #$grid->cache = array('use'=>0,'instance'=>Zend_Registry::get('cache'),'tag'=>'grid');
+                $grid = "Bvb_Grid_Deploy_Word";
                 break;
             case 'wordx' :
-                $grid = new Bvb_Grid_Deploy_Wordx ( $db, 'Grid Example', 'media/temp', array ('download' ) );
-                #$grid->cache = array('use'=>0,'instance'=>Zend_Registry::get('cache'),'tag'=>'grid');
+                $grid = "Bvb_Grid_Deploy_Wordx";
                 break;
             case 'pdf' :
-                $grid = new Bvb_Grid_Deploy_Pdf ( $db, 'Grid Example', 'media/temp', array ('download' ) );
-                #$grid->cache = array('use'=>0,'instance'=>Zend_Registry::get('cache'),'tag'=>'grid');
+                $grid = "Bvb_Grid_Deploy_Pdf";
                 break;
             case 'print' :
-                $grid = new Bvb_Grid_Deploy_Print ( $db, 'Grid Example' );
-                #$grid->cache = array('use'=>0,'instance'=>Zend_Registry::get('cache'),'tag'=>'grid');
+                $grid = "Bvb_Grid_Deploy_Print";
                 break;
             default :
-                $grid = new Bvb_Grid_Deploy_Table ( $db );
-                $grid->escapeOutput ( false );
-                $grid->addTemplateDir ( 'My/Template/Table', 'My_Template_Table', 'table' );
-                $grid->addElementDir ( 'My/Validate', 'My_Validate', 'validator' );
-                $grid->addElementDir ( 'My/Filter', 'My_Filter', 'filter' );
-                $grid->addFormatterDir ( 'My/Formatter', 'My_Formatter' );
-                $grid->imagesUrl = $this->getRequest ()->getBaseUrl () . '/public/images/';
-                $grid->cache = array ('use' => 0, 'instance' => Zend_Registry::get ( 'cache' ), 'tag' => 'grid' );
-                
+                $grid = "Bvb_Grid_Deploy_Table";
                 break;
         }
+        
+        $grid = new $grid (  $db, 'Grid Example', 'media/temp', array ('download' ) );
+        $grid->escapeOutput ( false );
+        $grid->addTemplateDir ( 'My/Template/Table', 'My_Template_Table', 'table' );
+        $grid->addElementDir ( 'My/Validate', 'My_Validate', 'validator' );
+        $grid->addElementDir ( 'My/Filter', 'My_Filter', 'filter' );
+        $grid->addFormatterDir ( 'My/Formatter', 'My_Formatter' );
+        $grid->imagesUrl = $this->getRequest ()->getBaseUrl () . '/public/images/';
+        $grid->cache = array ('use' => 0, 'instance' => Zend_Registry::get ( 'cache' ), 'tag' => 'grid' );
         
         return $grid;
     }
@@ -342,6 +334,7 @@ class SiteController extends Zend_Controller_Action
 
         #Zend_Debug::dump(Zend_Json::decode(file_get_contents('http://services.sapo.pt/JobOffers/JSON')));
         
+
         $grid->setDataFromCsv ( 'media/files/grid.csv' );
         #$grid->setDataFromXml ( $url, 'channel,item' );
         #$grid->removeColumns ( array ('guid', 'pubDate', 'link', 'author', 'category', 'title' ) );
@@ -363,7 +356,7 @@ class SiteController extends Zend_Controller_Action
         
         */
         
-        
+
         $this->view->pages = $grid->deploy ();
         
         $this->render ( 'index' );
