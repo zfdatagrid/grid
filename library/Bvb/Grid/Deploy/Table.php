89895f4aa04f24cd8befa8033a1611843656f55c
Bento Vilas Boas <bento@licentia.pt>
2010-04-21 20:10:20 +0000

    spelling

--- library/Bvb/Grid/Deploy/Table.php
+++ library/Bvb/Grid/Deploy/Table.php
@@ -1102,7 +1102,7 @@ class Bvb_Grid_Deploy_Table extends Bvb_Grid implements Bvb_Grid_Deploy_DeployIn
             }
 
             $url = $this->getUrl('perPage');
-            $menuPerPage = ' | '.$this->__('Show').' ' .$this->getView()->formSelect('perPage',$this->getParam('perPage',$this->_pagination),array('onChange'=>"window.location='$url/perPage".$this->getGridId()."/'+this.value;"),$this->_paginationOptions).' '. $this->__('itens');
+            $menuPerPage = ' | '.$this->__('Show').' ' .$this->getView()->formSelect('perPage',$this->getParam('perPage',$this->_pagination),array('onChange'=>"window.location='$url/perPage".$this->getGridId()."/'+this.value;"),$this->_paginationOptions).' '. $this->__('items');
         }else{
             $menuPerPage = '';
         }
