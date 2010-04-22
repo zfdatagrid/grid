94fc22aabfdad523dccce574487048835d87b7a2
Bento Vilas Boas <bento@licentia.pt>
2010-04-22 13:40:57 +0000

    Fixes: http://code.google.com/p/zfdatagrid/issues/detail?id=261
     getDistinctValuesForFilters mismatch with Interface

--- library/Bvb/Grid/Source/Array.php
+++ library/Bvb/Grid/Source/Array.php
@@ -325,7 +325,7 @@ class Bvb_Grid_Source_Array implements Bvb_Grid_Source_SourceInterface
     }
 
 
-    public function getDistinctValuesForFilters ($field, $fieldValue)
+    public function getDistinctValuesForFilters ($field, $value,$order = 'name ASC')
     {
 
         $filter = array();
