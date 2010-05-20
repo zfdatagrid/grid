6837b452d4bf21bc925d91fce6f37de01fe34c7d
Bento Vilas Boas <bento@licentia.pt>
2010-05-20 15:56:30 +0000

    Image name changes... seta_cima=>arrow_up, seta_baixo=>arrow_down

--- library/Bvb/Grid/Template/Table/Table.php
+++ library/Bvb/Grid/Template/Table/Table.php
@@ -168,7 +168,7 @@ class Bvb_Grid_Template_Table_Table
 
     public function images ($url)
     {
-        return array('asc' => "<img src=\"" . $url . "seta_cima.gif\" border=\"0\">", 'desc' => "<img src=\"" . $url . "seta_baixo.gif\" border=\"0\">", 'delete' => "<img src=\"" . $url . "delete.png\" border=\"0\">", 'detail' => "<img src=\"" . $url . "detail.png\" border=\"0\">", 'edit' => "<img src=\"" . $url . "edit.png\"  border=\"0\">");
+        return array('asc' => "<img src=\"" . $url . "arrow_up.gif\" border=\"0\">", 'desc' => "<img src=\"" . $url . "arrow_down.gif\" border=\"0\">", 'delete' => "<img src=\"" . $url . "delete.png\" border=\"0\">", 'detail' => "<img src=\"" . $url . "detail.png\" border=\"0\">", 'edit' => "<img src=\"" . $url . "edit.png\"  border=\"0\">");
     }
 
 
