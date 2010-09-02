4e10df081ed3742400bee63cfeae397d10766937
Bento Vilas Boas <bento@licentia.pt>
2010-09-02 22:19:19 +0000

    minor stuff

--- library/Bvb/Grid/Translator.php
+++ library/Bvb/Grid/Translator.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  *
  * LICENSE
@@ -17,35 +16,44 @@
  * @version    $Id$
  * @author     Bento Vilas Boas <geral@petala-azul.com >
  */
-
 class Bvb_Grid_Translator
 {
     protected static $_instance = null;
-
     /**
      * @var Zend_Translate
      */
     protected $_translator = null;
 
     final protected function __construct()
-    {
-    }
+    {}
 
+    /**
+     * Translator instante
+     * @return Zend_Translate
+     */
     public static function getInstance()
     {
         if (self::$_instance === NULL) {
             self::$_instance = new self();
         }
-
         return self::$_instance;
     }
 
+    /**
+     * Defines translator
+     * @param Zend_Translate $translator
+     * @return Zend_Translate
+     */
     public function setTranslator(Zend_Translate $translator)
     {
         $this->_translator = $translator;
         return $this;
     }
 
+    /**
+     * Returns current translator
+     * @return Zend_Translate
+     */
     public function getTranslator()
     {
         if ($this->_translator instanceof Zend_Translate) {
@@ -53,37 +61,41 @@ class Bvb_Grid_Translator
         } elseif (Zend_Registry::isRegistered('Zend_Translate')) {
             $this->_translator = Zend_Registry::get('Zend_Translate');
         }
-
         return $this->_translator;
     }
 
+    /**
+     * Translate a string
+     * @param $message
+     */
     public function __($message)
     {
         if (strlen($message) == 0) {
             return $message;
         }
-
         if ($this->getTranslator()) {
-            return $this->getTranslator()->translate($message);
+            return $this->getTranslator()
+                ->translate($message);
         }
-
         return $message;
     }
 
+    /**
+     * Checks if a string is translatable
+     * @param $message
+     */
     public function isTranslated($message)
     {
         if (strlen($message) == 0) {
             return false;
         }
-
         if ($this->getTranslator()) {
-            return $this->_translator->isTranslated($message);
+            return $this->_translator
+                ->isTranslated($message);
         }
-
         return false;
     }
 
     final protected function __clone()
-    {
-    }
+    {}
 }
