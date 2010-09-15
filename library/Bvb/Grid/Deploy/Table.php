b58241010f94cf9f3e0ce6abab2c4dd9fe1b64a7
Bento Vilas Boas <bento@licentia.pt>
2010-09-15 02:17:23 +0000

    FIXES: http://code.google.com/p/zfdatagrid/issues/detail?id=396

--- library/Bvb/Grid/Deploy/Table.php
+++ library/Bvb/Grid/Deploy/Table.php
@@ -2886,12 +2886,12 @@ function " . $this->getGridId() . "gridChangeFilters(event)
     {
         $pk = parent::setMassActions($options, $fields);
 
-       if(count($fields)>0)
-       {
-           $pk = "{{".implode('}}-{{',$fields)."}}";
-       }else{
-           $pk = "{{$pk}}";
-       }
+        if(count($fields)==0)
+        {
+            $fields = explode('-',$pk);
+        }
+
+        $pk = "{{".implode('}}-{{',$fields)."}}";
 
         $left = new Bvb_Grid_Extra_Column();
         $left->position('left')->title('')->name('ZFG_MASS_ACTIONS')->decorator("<input type='checkbox' onclick='observeCheckBox_" . $this->getGridId() . "(this)' name='gridMassActions_" . $this->getGridId() . "' id='massCheckBox_" . $this->getGridId() . "' value='".$pk."' >");
