f2b2376f517bcd14fe6093ab9f0161fc4a1d0065
Bento Vilas Boas <bento@licentia.pt>
2010-11-02 16:47:04 +0000

    notice supression

--- library/Bvb/Grid/Deploy/Table.php
+++ library/Bvb/Grid/Deploy/Table.php
@@ -734,12 +734,14 @@ class Bvb_Grid_Deploy_Table extends Bvb_Grid implements Bvb_Grid_Deploy_DeployIn
 
         $this->_actionsUrls['add'] = "$url/add" . $this->getGridId() . "/1";
 
-        if ($this->getSource()->hasCrud()) {
-            if (($this->getInfo('doubleTables') == 0 && $this->_allowAdd == 1 ) && $this->getSource()->getPrimaryKey($this->_data['table']) &&  $this->_allowAddButton == 1 && $this->getParam('add')!=1 && $this->getParam('edit')!=1) {
-                $addButton = "<button class='addRecord' onclick=\"window.location='".$this->_actionsUrls['add']."';\">" . $this->__('Add Record') . "</button>";
-            }else{
+        if ( $this->getSource()->hasCrud() ) {
+            if ( ($this->getInfo('doubleTables') == 0 && $this->_allowAdd == 1) && $this->getSource()->getPrimaryKey($this->_data['table']) && $this->_allowAddButton == 1 && $this->getParam('add') != 1 && $this->getParam('edit') != 1 ) {
+                $addButton = "<button class='addRecord' onclick=\"window.location='" . $this->_actionsUrls['add'] . "';\">" . $this->__('Add Record') . "</button>";
+            } else {
                 $addButton = '';
             }
+        } else {
+            $addButton = '';
         }
 
         /**
