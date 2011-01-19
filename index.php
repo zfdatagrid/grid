9a4341c0514e8b7555a0f058383a7dab8d2ba8d3
Bento Vilas Boas <bento@licentia.pt>
2011-01-19 00:59:38 +0000

    Version 0.7

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
 error_reporting(-1);
-ini_set('display_errors',1);
-ini_set('max_execution_time',0);
+ini_set('display_errors', 1);
+ini_set('max_execution_time', 0);
 
-date_default_timezone_set ( 'Europe/Lisbon' );
-set_include_path ( '.' . PATH_SEPARATOR . './library/'
-.'.' . PATH_SEPARATOR . './application/'
-. PATH_SEPARATOR . get_include_path () );
+date_default_timezone_set('Europe/Lisbon');
+set_include_path('.' . PATH_SEPARATOR . './library/'
+        . '.' . PATH_SEPARATOR . './application/'
+        . PATH_SEPARATOR . get_include_path());
 
 include "Zend/Loader/Autoloader.php";
 
@@ -20,69 +20,69 @@ $autoloader->setFallbackAutoloader(true);
 Zend_Session::start();
 
 // Load Config
-$config = new Zend_Config_Ini ( './application/config.ini', 'general' );
-Zend_Registry::set ( 'config', $config );
+$config = new Zend_Config_Ini('./application/config.ini', 'general');
+Zend_Registry::set('config', $config);
 
 // Database
-$db = Zend_Db::factory ( $config->db->adapter, $config->db->config->toArray () );
-Zend_Db_Table::setDefaultAdapter ( $db );
+$db = Zend_Db::factory($config->db->adapter, $config->db->config->toArray());
+Zend_Db_Table::setDefaultAdapter($db);
 #$db->getConnection ()->exec ( "SET NAMES utf8" );
-$db->setFetchMode ( Zend_Db::FETCH_OBJ );
+$db->setFetchMode(Zend_Db::FETCH_OBJ);
 $db->setProfiler(true);
-Zend_Registry::set ( 'db', $db );
+Zend_Registry::set('db', $db);
 
 //Cache Options
-$frontendOptions = array('lifetime' => 7200,'automatic_serialization' => true);
+$frontendOptions = array('lifetime' => 7200, 'automatic_serialization' => true);
 $backendOptions = array('cache_dir' => './data/cache/');
 $cache = Zend_Cache::factory('Core', 'File', $frontendOptions, $backendOptions);
-Zend_Registry::set('cache',$cache);
+Zend_Registry::set('cache', $cache);
 
 //Locale
-$locale = new Zend_Locale ( 'en_US' );
-Zend_Registry::set ( 'locale', $locale );
+$locale = new Zend_Locale('en_US');
+Zend_Registry::set('locale', $locale);
 /*
-      $english = array(
-          'Name_of' => 'Barcelos',
-          'message2 => 'message2',
-          'message3' => 'message3');
+  $english = array(
+  'Name_of' => 'Barcelos',
+  'message2 => 'message2',
+  'message3' => 'message3');
 
-      $german = array(
-          'Fmessage1 => 'Nachricht1',
-          'message2' => 'Nachricht2',
-          'message3' => 'Nachricht3');
+  $german = array(
+  'Fmessage1 => 'Nachricht1',
+  'message2' => 'Nachricht2',
+  'message3' => 'Nachricht3');
 
-      $translate = new Zend_Translate('array', $english, 'en');
+  $translate = new Zend_Translate('array', $english, 'en');
 
-      Zend_Registry::set('Zend_Translate',$translate);
-*/
+  Zend_Registry::set('Zend_Translate',$translate);
+ */
 $frontController = Zend_Controller_Front::getInstance();
 $frontController->throwExceptions(true);
 $frontController->setControllerDirectory('./application/controllers');
 $frontController->setDefaultControllerName('site');
 
 /*
-      $route = new Zend_Controller_Router_Route(
-          'grid/*',
-          array(
-              'controller' => 'site',
-              'action'     => 'basic'
-          )
-      );
+  $route = new Zend_Controller_Router_Route(
+  'grid/*',
+  array(
+  'controller' => 'site',
+  'action'     => 'basic'
+  )
+  );
 
-      $router = $frontController->getRouter();
-      $router->addRoute('user', $route);
-*/
+  $router = $frontController->getRouter();
+  $router->addRoute('user', $route);
+ */
 // Leave 'Database' options empty to rely on Zend_Db_Table default adapter
 $options = array(
     'jquery_path' => 'http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js',
     'plugins' => array('Variables',
-                       'Html',
-                       'Database' => array('adapter' => array('standard' => $db)),
-                       'File' => array('base_path' => '/Library/WebServer/Documents/'),
-                       'Memory',
-                       'Time',
-                       #'Cache' => array('backend' => $cache->getBackend()),
-                       'Exception')
+        'Html',
+        'Database' => array('adapter' => array('standard' => $db)),
+        'File' => array('base_path' => '/Library/WebServer/Documents/'),
+        'Memory',
+        'Time',
+        #'Cache' => array('backend' => $cache->getBackend()),
+        'Exception')
 );
 
 $debug = new ZFDebug_Controller_Plugin_Debug($options);
@@ -90,4 +90,4 @@ $debug = new ZFDebug_Controller_Plugin_Debug($options);
 $frontController = Zend_Controller_Front::getInstance();
 #$frontController->registerPlugin($debug);
 
-$frontController->dispatch ();
+$frontController->dispatch();
