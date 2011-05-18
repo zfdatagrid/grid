beefed7fcacd2df8f2be0515a458e77265dfdb82
Bento Vilas Boas <bento@licentia.pt>
2011-05-18 17:41:52 +0000

    FIXES: http://code.google.com/p/zfdatagrid/issues/detail?id=669

--- library/Bvb/Grid/Source/Zend/Select.php
+++ library/Bvb/Grid/Source/Zend/Select.php
@@ -1183,6 +1183,10 @@ class Bvb_Grid_Source_Zend_Select extends Bvb_Grid_Source_Db_DbAbstract implemen
     public function buildForm($inputsType = array())
     {
         $table = $this->getMainTable();
+        if(!isset($table['schema']))
+        {
+            $table['schema'] ='';
+        }
         $cols = $this->getDescribeTable($table['table'], $table['schema']);
 
         return $this->buildFormElements($cols, array(), $inputsType, array());
