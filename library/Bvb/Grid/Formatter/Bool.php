6d285a2bf4e8897adf16b4c5655bb1b5294e5c6b
Bento Vilas Boas <bento@licentia.pt>
2011-05-19 00:12:26 +0000

    small issue with models and crud

---
+++ library/Bvb/Grid/Formatter/Bool.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license
+ * It is  available through the world-wide-web at this URL:
+ * http://www.petala-azul.com/bsd.txt
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to geral@petala-azul.com so we can send you a copy immediately.
+ *
+ * @package   Bvb_Grid
+ * @author    Bento Vilas Boas <geral@petala-azul.com>
+ * @copyright 2010 ZFDatagrid
+ * @license   http://www.petala-azul.com/bsd.txt   New BSD License
+ * @version   $Id$
+ * @link      http://zfdatagrid.com
+ */
+
+class Bvb_Grid_Formatter_Bool implements Bvb_Grid_Formatter_FormatterInterface
+{
+
+    protected $_locale = null;
+
+    /**
+     * Constructor
+     * @param array $options
+     */
+    public function __construct($options = array())
+    {
+       
+    }
+
+    /**
+     * Formats a given value
+     * @see library/Bvb/Grid/Formatter/Bvb_Grid_Formatter_FormatterInterface::format()
+     */
+    public function format($value)
+    {
+        $translate = Bvb_Grid_Translator::getInstance();
+
+        return ((bool)($value))?$translate->__('Yes'):$translate->__('No');
+    }
+}
